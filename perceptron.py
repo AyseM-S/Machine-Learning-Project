5f7242db5026d9b02abdce6e7085bfb60820204a
Nur171 <nuraksoy2023@gmail.com>
2025-12-07 16:14:18 +0300

    new
    Refactor Perceptron class to PerceptronModel and improve evaluation metrics return format.

--- perceptron.py
+++ perceptron.py
@@ -1,8 +1,8 @@
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
-class Perceptron :
-    def _init_ (self):
+class PerceptronModel :
+    def __init__ (self):
         self.model = Perceptron()
 
     def fit (self, X_train, y_train):
@@ -10,12 +10,16 @@ class Perceptron :
 
     def predict(self,X_test):
         return self.model.predict(X_test)
-
+    
     def evaluate(self, y_true, y_pred):
-        accuracy = accuracy_score(y_true, y_pred)  
-        precision = precision_score(y_true, y_pred, average = "macro", zero_division = 0) 
-        recall = recall_score(y_true, y_pred, average = "macro", zero_division = 0) 
-        f1 = f1_score(y_true, y_pred, average = "macro", zero_division = 0)
-        confusion_matrix = confusion_matrix(y_true,y_pred)
 
-        return "Accuracy: " + accuracy + "\nPrecision: " + precision + "\nRecall: " + recall + "\nF1: " + f1 + "\nConfusion_matrix: " + confusion_matrix
+        metrics = {
+            "Accuracy": accuracy_score(y_true, y_pred),
+            "Precision": precision_score(y_true, y_pred, average="macro", zero_division=0),
+            "Recall": recall_score(y_true, y_pred, average="macro", zero_division=0),
+            "F1": f1_score(y_true, y_pred, average="macro", zero_division=0),
+            "Confusion_matrix": confusion_matrix(y_true, y_pred).tolist()
+        }
+
+        return metrics
+
