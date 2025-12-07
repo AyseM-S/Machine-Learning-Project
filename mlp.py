53075890f5c3bb76c891d1568891f4e682186c67
Nur171 <nuraksoy2023@gmail.com>
2025-12-07 16:16:04 +0300

    new
    Refactor MLP class to MLP_Model and improve evaluation metrics return format.

--- mlp.py
+++ mlp.py
@@ -2,7 +2,28 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 
-class MLP:
+class MLP_Model:
+
+    def __init__(self, random_state=42):
+        self.model = MLPClassifier(
+            hidden_layer_sizes=(64, 32),
+            activation='relu',
+            solver='adam',
+            max_iter=300,
+            random_state=random_state
+        )
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
+
+    from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+
+
+class MLP_Model:
 
     def __init__(self, random_state=42):
         self.model = MLPClassifier(
@@ -20,11 +41,14 @@ class MLP:
         return self.model.predict(X_test)
 
     def evaluate(self, y_true, y_pred):
-        accuracy = accuracy_score(y_true, y_pred)  
-        precision = precision_score(y_true, y_pred, average = "macro", zero_division = 0) 
-        recall = recall_score(y_true, y_pred, average = "macro", zero_division = 0) 
-        f1 = f1_score(y_true, y_pred, average = "macro", zero_division = 0)
-        confusion_matrix = confusion_matrix(y_true,y_pred)
 
-        return "Accuracy: " + accuracy + "\nPrecision: " + precision + "\nRecall: " + recall + "\nF1: " + f1 + "\nConfusion_matrix: " + confusion_matrix
+        metrics = {
+            "Accuracy": accuracy_score(y_true, y_pred),
+            "Precision": precision_score(y_true, y_pred, average="macro", zero_division=0),
+            "Recall": recall_score(y_true, y_pred, average="macro", zero_division=0),
+            "F1": f1_score(y_true, y_pred, average="macro", zero_division=0),
+            "Confusion_matrix": confusion_matrix(y_true, y_pred).tolist()
+        }
+
+        return metrics
 
