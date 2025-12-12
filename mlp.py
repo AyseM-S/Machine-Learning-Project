f591f2a09fd38b119bad3d8f45a826619304852c
Nur171 <nuraksoy2023@gmail.com>
2025-12-12 15:47:18 +0300

    new

--- mlp.py
+++ mlp.py
@@ -4,14 +4,17 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 class MLP_Model:
 
-    def __init__(self, random_state=42):
+   class MLP_Model:
+
+    def __init__(self, hidden_layers=(64, 32), random_state=42):
         self.model = MLPClassifier(
-            hidden_layer_sizes=(64, 32),
+            hidden_layer_sizes=hidden_layers,
             activation='relu',
             solver='adam',
             max_iter=300,
             random_state=random_state
         )
+
 
     def fit(self, X_train, y_train):
         self.model.fit(X_train, y_train)
@@ -51,4 +54,5 @@ class MLP_Model:
         }
 
         return metrics
+
 
